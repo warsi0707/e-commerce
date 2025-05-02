7351e5254d5fc39f457e9e05689f85a35a7502ec
samir <samir.warsi2001@gmail.com>
2025-05-02 15:41:27 +0530

    changes

--- frontend/src/BckendUrl.jsx
+++ frontend/src/BckendUrl.jsx
@@ -1,1 +1,2 @@
-export const Backendurl = 'http://localhost:3000/api/v1'
+// export const Backendurl = 'http://localhost:3000/api/v1'
+export const Backendurl = 'https://e-cart-b.onrender.com/api/v1'
